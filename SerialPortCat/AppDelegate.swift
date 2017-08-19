32a298333f0ff3cb2fae85d9b5cc4a7f26518f41
Bijiabo <bijiabo@gmail.com>
2017-08-19 23:44:23 +0800

    Initial Commit

---
+++ SerialPortCat/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SerialPortCat
+//
+//  Created by huchunbo on 2017/8/19.
+//  Copyright © 2017年 huchunbo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SerialPortCat/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SerialPortCat
+//
+//  Created by huchunbo on 2017/8/19.
+//  Copyright © 2017年 huchunbo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
